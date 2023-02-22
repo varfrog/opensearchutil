a31f4a7ca7bf7c41658313ed4ceca3c7d8b4053d
Arnoldas Grigutis <x75w75@gmail.com>
2023-02-22 18:50:32 +0200

    Add a test case

--- request_body_builder_test.go
+++ request_body_builder_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestRequestBodyBuilder_BuildIndexBody(t *testing.T) {
+func TestRequestBodyBuilder_BuildIndexBody_NestedStruct(t *testing.T) {
 	g := gomega.NewGomegaWithT(t)
 	builder := NewRequestBodyBuilder()
 
@@ -47,6 +47,36 @@ func TestRequestBodyBuilder_BuildIndexBody(t *testing.T) {
 {"id":"680","name":"Ann","age":40,"address":{"postal_code":10000}}
 {"index":{"_index":"people","_id":"730"}}
 {"id":"730","name":"Bob","age":38,"address":{"postal_code":35000}}
+`
+	g.Expect(resBody).To(gomega.Equal(expBody))
+}
+
+func TestRequestBodyBuilder_BuildIndexBody_Pointers(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+	builder := NewRequestBodyBuilder()
+
+	type color struct {
+		Name string `json:"name"`
+	}
+	type person struct {
+		Name     string `json:"name"`
+		FavColor *color `json:"fav_color"`
+	}
+
+	resBody, err := builder.BuildIndexBody([]ObjectWrapper{
+		{
+			ID:    "1",
+			Index: "people",
+			Object: &person{
+				Name:     "Ann",
+				FavColor: &color{Name: "Yellow"},
+			},
+		},
+	})
+	g.Expect(err).To(gomega.BeNil())
+
+	expBody := `{"index":{"_index":"people","_id":"1"}}
+{"name":"Ann","fav_color":{"name":"Yellow"}}
 `
 	g.Expect(resBody).To(gomega.Equal(expBody))
 }
